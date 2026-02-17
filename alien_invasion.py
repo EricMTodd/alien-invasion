b07dec302b11de9d3b06d1ab24edeb5dea4b2969
EricMTodd <eterictodd@gmail.com>
2026-02-17 11:35:56 -0700

    Game now dynamically speeds up after each round. Working on implementing scoring.

--- alien_invasion.py
+++ alien_invasion.py
@@ -73,7 +73,9 @@ class AlienInvasion:
         """Start a new game when the player clicks Play."""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.game_active:
-            #Reset the game statistics.
+            # Reset the game settings.
+            self.settings.initialize_dynamic_settings()
+            # Reset the game statistics.
             self.stats.reset_stats()
             self.game_active = True
 
@@ -134,6 +136,7 @@ class AlienInvasion:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _check_aliens_bottom(self):
         """Check if any aliens have reached the bottom of the screen."""
